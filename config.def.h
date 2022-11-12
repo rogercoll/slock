dc6e4ed42b4227e24e2bd7635651662f003beb46
Roger Coll <rogercoll@protonmail.com>
2022-11-12 17:19:43 +0100

    feat: use neck username

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "neck";
+static const char *group = "neck";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
